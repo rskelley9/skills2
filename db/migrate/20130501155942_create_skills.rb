671956da3fc7aec765b5c1fda3de2530b5a7b369
Ryan Kelley <rskelley9@gmail.com>
2013-10-17 12:42:48 -0500

    Update 20130501155942_create_skills.rb

--- db/migrate/20130501155942_create_skills.rb
+++ db/migrate/20130501155942_create_skills.rb
@@ -3,8 +3,6 @@ class CreateSkills < ActiveRecord::Migration
     create_table :skills do |t|
       t.string :name
       t.string :context
-      t.integer :user_id
-
       t.timestamps
     end
   end
